5a3d1a38db552a7d780255fd5475e4600aee75a7
Laurent David <lmedavid@gmail.com>
2022-03-31 17:57:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+IMTPN Boost Based theme
+==
+
+This is a child theme for the "Pédagothèque Numérique" for IMT.
+
+
+
+Logos
+==
+
+Images
+==
+
+Images are mostly coming from:
+
+* Unsplash : https://unsplash.com/ (https://unsplash.com/license)
+* Pexels : https://www.pexels.com/fr-fr/license
+* Logos: https://www.imt-telecom_sud_paris.fr/fr/l-ecole/logotypes-imt-telecom_sud_paris
---
+++ lib.php
@@ -0,0 +1,57 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme plugin additional customisation
+ *
+ * @package   theme_imtpn
+ * @copyright 2021 - CALL Learning - Laurent David <laurent@call-learning.fr>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+use theme_imtpn\mur_pedagogique;
+
+/**
+ * Serves any files associated with the theme settings.
+ *
+ * @param stdClass $course
+ * @param stdClass $cm
+ * @param context $context
+ * @param string $filearea
+ * @param array $args
+ * @param bool $forcedownload
+ * @param array $options
+ * @return bool
+ * @throws coding_exception
+ */
+function theme_imtpn_telecom_sud_paris_pluginfile($course, $cm, $context, $filearea, $args, $forcedownload, array $options = array()) {
+    return theme_clboost\local\utils::generic_pluginfile('imtpn', $course, $cm, $context, $filearea, $args, $forcedownload,
+        $options);
+}
+
+
+/**
+ * Fix issue with notloggedin class
+ *
+ * Usually the pages are marked as notlogged in if no user is logged in. In case the guest user
+ * is logged in, the notloggedin is not there anymore, resulting in the left navbar taking space.
+ * This resolves this issue on this theme.
+ *
+ * @param $page
+ */
+function theme_imtpn_telecom_sud_paris_page_init($page) {
+    mur_pedagogique::set_additional_page_classes($page);
+}
